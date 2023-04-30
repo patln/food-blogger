36fe5264153ed05a41173f1edcf44f2b9b649f3a
patln <patricklin86@gmail.com>
2023-04-30 14:18:06 -0700

    Move title header from App to new component, Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return <h1>{this.props.title}</h1>
+    }
+}
+
+export default Header;
